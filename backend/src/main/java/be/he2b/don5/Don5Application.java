1074d41d4415f1b78a8affe7436356cd59654fb2
62727 <62727@etu.he2b.be>
2025-12-24 16:08:52 +0100

    feat: kafka working

--- backend/src/main/java/be/he2b/don5/Don5Application.java
+++ backend/src/main/java/be/he2b/don5/Don5Application.java
@@ -2,10 +2,12 @@ package be.he2b.don5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableKafka
 public class Don5Application {
 
 	public static void main(String[] args) {
