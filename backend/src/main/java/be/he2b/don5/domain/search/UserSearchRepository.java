6939418b39bb15416cd7b7004066a46a9ad45045
62294 <62294@etu.he2b.be>
2026-07-22 19:35:55 +0200

    remove: outdated files

--- backend/src/main/java/be/he2b/don5/domain/search/UserSearchRepository.java
+++
@@ -1,9 +0,0 @@
-package be.he2b.don5.domain.search;
-
-import java.util.List;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface UserSearchRepository extends ElasticsearchRepository<UserSearchDocument, String> {
-    List<UserSearchDocument> findByNameContainingIgnoreCaseOrBioContainingIgnoreCase(String name, String bio);
-    List<UserSearchDocument> findByInterestsIn(List<String> interests);
-}
